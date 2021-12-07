d9707d5da71566da697df930d9ddf5123b921e8c
Johnny Cache <johnycsh@gmail.com>
2021-12-06 23:48:47 -0800

    Initial jmp address set: 0x8000

--- gen_rom_01.py
+++ gen_rom_01.py
@@ -2,5 +2,7 @@
 # to program: minipro -p AT28C256 -w rom.bin 
 rom = bytearray( [0xea] * 32768)
 
+rom[0x7ffc] = 0x00 #Little endian: 0x8000 -> 0x10000000
+rom[0x7ffd] = 0x80 #                 A15   ----^
 with open("rom.bin", "wb") as out_file:
     out_file.write(rom)
