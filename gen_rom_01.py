0a2ed8b00f5859f2b1dd4c6aed9f467fa4c8928c
Johnny Cache <johnycsh@gmail.com>
2021-12-06 23:58:12 -0800

    OUT 0x6000, 0x42

--- gen_rom_01.py
+++ gen_rom_01.py
@@ -2,6 +2,17 @@
 # to program: minipro -p AT28C256 -w rom.bin 
 rom = bytearray( [0xea] * 32768)
 
+#Fill A register with immediate 0x42
+rom[0] = 0xa9   #; LDA 0x42
+rom[1] = 0x42   #; 
+
+# Store the contents of A register at address 0x6000
+rom[2] = 0x8d   #; OUT 0x6000, 0x42
+rom[3] = 0x00   #; at  
+rom[4] = 0x60   #;
+
+# What happens on next CLK cycle? 
+
 rom[0x7ffc] = 0x00 #Little endian: 0x8000 -> 0x10000000
 rom[0x7ffd] = 0x80 #                 A15   ----^
 with open("rom.bin", "wb") as out_file:
